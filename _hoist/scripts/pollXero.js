cffdd05845fb045f99d5b5a49a672af34a999640
Owen Evans <owen@bgeek.net>
2015-02-27 09:24:08 -0800

    log error

--- _hoist/scripts/pollXero.js
+++ _hoist/scripts/pollXero.js
@@ -5,11 +5,14 @@
 
 
 module.exports = function (ev, done) {
-  Hoist.log('in poll xero', ev);
+  Hoist.log('in poll xero');
 
   var connector = Hoist.connector('xero');
   return connector.get('/contacts')
     .then(function (result) {
       Hoist.log('got response from xero', result);
+    }).catch(function (err) {
+      Hoist.log(err);
+      throw err;
     }).nodeify(done);
 };
