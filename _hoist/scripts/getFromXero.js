15a5c86300a855900b2ffd61a5f1bac12dd17610
Owen Evans <owen@bgeek.net>
2015-08-10 18:48:22 -0700

    wat?

--- _hoist/scripts/getFromXero.js
+++ _hoist/scripts/getFromXero.js
@@ -2,7 +2,7 @@
 module.exports = function () {
   console.log(Hoist.connector);
   var partner = Hoist.connector('xero-public');
-  return partner.init().then(function (p) {
-    return p.get('/invoices');
+  return partner.init().then(function () {
+    return partner.get('/invoices');
   });
 };
