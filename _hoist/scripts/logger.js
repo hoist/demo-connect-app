30f95b8d67b276ffb882f327f74c0f3bdec7b7f3
Owen Evans <owen@bgeek.net>
2015-08-18 13:54:35 +1200

    add some timings for event loops

--- _hoist/scripts/logger.js
+++ _hoist/scripts/logger.js
@@ -1,4 +1,20 @@
+var bluebird = require('bluebird');
 module.exports = function (ev, done) {
   Hoist.log('hello');
-  return Hoist.event.raise('an:event');
+  var i = 0;
+  var promises = [];
+  var globalStart = process.hrtime();
+  while (i < 100) {
+    var index = i;
+    var startTime = process.hrtime();
+    promises.push(Hoist.event.raise('an:event').then(function () {
+      var diff = process.hrtime(startTime);
+      console.log('raise for ' + i + ' took %d nanoseconds', diff[0] * 1e9 + diff[1]);
+
+    }));
+  }
+  return bluebird.allSettled(promises).then(function () {
+    var diff = process.hrtime(globalStart);
+    console.log('raise for all took %d nanoseconds', diff[0] * 1e9 + diff[1]);
+  });
 }
