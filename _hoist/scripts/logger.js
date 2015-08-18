e28f14fbb3bab887b42fa15c2e1c849813068176
Owen Evans <owen@bgeek.net>
2015-08-18 13:58:01 +1200

    try reducing for simplicity

--- _hoist/scripts/logger.js
+++ _hoist/scripts/logger.js
@@ -4,17 +4,14 @@ module.exports = function (ev, done) {
   var i = 0;
   var promises = [];
   var globalStart = process.hrtime();
-  while (i < 100) {
-    var index = i;
-    var startTime = process.hrtime();
-    promises.push(Hoist.event.raise('an:event').then(function () {
-      var diff = process.hrtime(startTime);
-      console.log('raise for ' + i + ' took %d nanoseconds', diff[0] * 1e9 + diff[1]);
 
-    }));
-  }
-  return bluebird.allSettled(promises).then(function () {
-    var diff = process.hrtime(globalStart);
-    console.log('raise for all took %d nanoseconds', diff[0] * 1e9 + diff[1]);
+  var index = i;
+  var startTime = process.hrtime();
+  return Hoist.event.raise('an:event').then(function () {
+    var diff = process.hrtime(startTime);
+    console.log('raise for ' + i + ' took %d nanoseconds', diff[0] * 1e9 + diff[1]);
+
   });
+
+
 }
