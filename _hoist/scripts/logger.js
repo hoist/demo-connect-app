6292b3787ba4758b7ea9dc3116c3328605efbaa7
Owen Evans <owen@bgeek.net>
2015-08-18 14:03:51 +1200

    fix logging

--- _hoist/scripts/logger.js
+++ _hoist/scripts/logger.js
@@ -1,21 +1,25 @@
 var bluebird = require('bluebird');
 module.exports = function (ev, done) {
   Hoist.log('hello');
-  var i = 0;
   var promises = [];
   var globalStart = process.hrtime();
 
-  var index = i;
-  var startTime = process.hrtime();
   return Bluebird.all(
-    Array(100).map(function (index) {
-      return Hoist.event.raise('an:event').then(function () {
-        var diff = process.hrtime(startTime);
-        console.log('raise for ' + index + ' took %d nanoseconds', diff[0] * 1e9 + diff[1]);
-      });
-    })).then(function () {
-    console.log('all done');
-  });
+      Array(100).map(function (index) {
+        var startTime = process.hrtime();
+        return Hoist.event.raise('an:event').then(function () {
+          var diff = process.hrtime(startTime);
+          console.log('raise for ' + index + ' took %d nanoseconds', diff[0] * 1e9 + diff[1]);
+        }).catch(function (err) {
+          console.log('error', err);
+        });
+      }))
+    .then(function () {
+      var diff = process.hrtime(globalStart);
+      console.log('all done in %d nanoseconds', diff[0] * 1e9 + diff[1]);
+    }).catch(function (err) {
+      console.log('error', err);
+    });
 
 
 }
